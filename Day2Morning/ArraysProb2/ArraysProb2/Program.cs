44948dfeb8a2f45175f99da0e9777f424aaaeb90
cts466849 <srinivasa.c@cognizant.com>
2015-10-14 14:30:30 +0530

    ArraysProb2

---
+++ Day2Morning/ArraysProb2/ArraysProb2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraysProb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] newArray = new int[4, 2] { { 1, 3 }, { 15, 7 }, { 80, 2 }, { 99, 1 } };
+            PrintTheArray(newArray);
+            Console.Read();
+        }
+
+        static void PrintTheArray(Array theArray)
+        {
+            foreach (var e in theArray)
+                Console.WriteLine(e);
+        }
+
+    }
+}

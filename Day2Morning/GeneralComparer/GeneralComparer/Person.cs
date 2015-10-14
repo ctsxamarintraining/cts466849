42e74e0bc1dca464130472fe6b2be79a398c8aaa
cts466849 <srinivasa.c@cognizant.com>
2015-10-14 15:00:25 +0530

    General Comparer
    initial commit

---
+++ Day2Morning/GeneralComparer/GeneralComparer/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace GeneralComparer
+{
+    public class Person
+    {
+        public string name { set; get; }
+        public int age { set; get; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public static IComparer SortByName
+        {
+            get
+            {
+                return ((IComparer)new SortByNameClass());
+            }
+        }
+
+
+        class SortByNameClass : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                Person p1 = (Person)obj1;
+                Person p2 = (Person)obj2;
+
+                return (String.Compare(p1.name, p2.name));
+            }
+        }
+
+    }
+}
---
+++ Day2Morning/GeneralComparer/GeneralComparer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] pArray = new Person[4];
+
+            pArray[0] = new Person("SRINIVAS", 24);
+            pArray[1] = new Person("SUNIL", 26);
+            pArray[2] = new Person("SOWMYA", 25);
+            pArray[3] = new Person("RAMYA", 20);
+
+
+            Array.Sort(pArray, Person.SortByName);
+
+            for (int i = 0; i < pArray.Length; i++)
+            {
+
+                Console.WriteLine(pArray[i].name);
+            }
+            Console.Read();
+        }
+    }
+}

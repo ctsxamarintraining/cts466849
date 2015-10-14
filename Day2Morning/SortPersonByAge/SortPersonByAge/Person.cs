13a4cf5d3c2da611b540c831905645678eb4769c
cts466849 <srinivasa.c@cognizant.com>
2015-10-14 14:50:50 +0530

    Day2Morning

---
+++ Day2Morning/SortPersonByAge/SortPersonByAge/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortPersonByAge
+{
+    class Person : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName {get;set;}
+        public int Age {get;set;}
+
+        public Person()
+        {
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is Person)
+            {
+                return this.Age.CompareTo((obj as Person).Age);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day2Morning/SortPersonByAge/SortPersonByAge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortPersonByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3];
+
+            Person p1 = new Person();
+            p1.FirstName = "Srinivas";
+            p1.Age = 1;
+            p1.LastName = "C";
+            person[0] = p1;
+
+            Person p2 = new Person();
+            p2.FirstName = "Sunil";
+            p2.Age = 25;
+            p2.LastName = "M";
+            person[1] = p2;
+
+            Person p3 = new Person();
+            p3.FirstName = "Sowmya";
+            p3.Age = 24;
+            p3.LastName = "R";
+            person[2] = p3;
+
+            Array.Sort(person);
+            for (int i = 0; i < person.Length; i++)
+            {
+                if (person[i].Age != null)
+                Console.WriteLine(person[i].Age);
+
+            }
+            Console.Read();
+
+        }
+    }
+}
